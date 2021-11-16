c5a7e7eeb318f1302700e1616a2a408ff3b1a381
Simen A. W. Olsen <so@bjerk.io>
2021-11-16 11:11:53 +0100

    use React.forwardRef

--- src/skeleton.tsx
+++ src/skeleton.tsx
@@ -1,7 +1,8 @@
 import { keyframes } from '@emotion/react';
 import React from 'react';
-import { Box } from 'theme-ui';
-export interface SkeletonProps {
+import { Box, BoxProps } from 'theme-ui';
+
+export interface SkeletonProps extends BoxProps {
   /** `circular` | `rectangular` | `text` */
   shape: 'circular' | 'rectangular' | 'text';
 }
@@ -15,25 +16,22 @@ const pulse = keyframes({
   },
 });
 
-export const Skeleton = React.FC<SkeletonProps>(({ shape }: SkeletonProps) => {
-  if (shape === 'text')
-    return (
-      <Box
-        ref={ref}
-        variant="primary"
-        {...props}
-        // @ts-expect-error themeKey is a private prop
-        __themeKey="skeleton"
-        __css={{
-          height: '1rem',
-          borderRadius: 4,
-          backgroundColor: 'muted',
-        }}
-        sx={{
-          animationName: pulse,
-          animationDuration: '1s',
-          animationIterationCount: 'infinite',
-        }}
-      />
-    );
-});
+export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ shape = 'text', ...props }, ref) => (
+    <Box
+      ref={ref}
+      variant="primary"
+      {...props}
+      // @ts-expect-error themeKey is a private prop
+      __themeKey="skeleton"
+      __css={{
+        height: shape === 'circular' ? '1rem' : '2rem',
+        borderRadius: 4,
+        backgroundColor: 'muted',
+        animationName: pulse,
+        animationDuration: '1s',
+        animationIterationCount: 'infinite',
+      }}
+    />
+  ),
+);
